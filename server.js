0d3cb388e5a6ecfb863ab79b9cc236d09571c805
coderismail22 <122710226+coderismail22@users.noreply.github.com>
2023-08-07 05:51:08 +0600

    middleware-configured

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.VITE_PAYMENT_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Middleware configuration:
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+};
+
+app.use(cors(corsConfig));
+app.use(express.json());
