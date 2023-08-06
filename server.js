d2a511469679bbb35c589e3667b7d44497a85d54
coderismail22 <122710226+coderismail22@users.noreply.github.com>
2023-08-07 05:50:32 +0600

    port-configured

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.VITE_PAYMENT_SECRET_KEY);
+const app = express();
+const port = process.env.PORT || 5000;
