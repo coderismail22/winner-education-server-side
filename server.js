6fb55f105e0b076b136511aa3594769772bd5b33
coderismail22 <122710226+coderismail22@users.noreply.github.com>
2023-08-07 05:49:52 +0600

    stripe-added

--- server.js
+++ server.js
@@ -2,3 +2,4 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.VITE_PAYMENT_SECRET_KEY);
